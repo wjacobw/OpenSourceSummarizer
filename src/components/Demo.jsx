21d1a73615adf78b1cf93b13fe07292faf1bd9bd
wjacobw <williamjacob0910@gmail.com>
2023-09-15 21:26:13 +0800

    Adds the main User Interface of the website

---
+++ src/components/Demo.jsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react';
+
+import {copy, linkIcon, loader, tick} from '../assets';
+
+const Demo = () => {
+  //useState field, shortcut: useState
+  const [article, setArticle] = useState({
+    url: '',
+    summary: '',
+  })
+
+  const handleSubmit = async (e) => {
+    alert('submitted')
+  }
+
+
+
+  return <section className="mt-16 w-full max-w-xl"> 
+    <div className="flex flex-col w-full gap-2">
+      <form className="relative flex justify-center items-center"
+      onSubmit={handleSubmit}>
+        <img src= {linkIcon} alt="link_icon" 
+        className="absolute left-0 my-2 ml-3 w-5"/>
+        <input type="url" placeholder="Enter a URL" 
+        value = {article.url} 
+        onChange = {(e) => setArticle({ ...article, url: e.target.value})} required className = "url_input peer"/> 
+        <button type="submit" className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">go</button>
+      </form>
+    </div>
+    </section>;
+};
+
+export default Demo;
