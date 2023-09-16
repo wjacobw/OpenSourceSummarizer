2c09f20a09c4e8b4bbeff5c5ceb86b29845242d6
wjacobw <williamjacob0910@gmail.com>
2023-09-16 13:48:06 +0800

    Adds the final features of the website
    Adds the API call and more UI features

--- src/services/article.js
+++ src/services/article.js
@@ -1,10 +1,24 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
+const rapidApiKey = import.meta.env.VITE_KEY;
+console.log(1)
 export const articleApi = createApi( {
     reducerPath: 'articleApi',
+    baseQuery: fetchBaseQuery({baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/', 
+prepareHeaders: (headers) => {
+headers.set('X-RapidAPI-Key', 
+'7774cb4961msh1657901529b28c8p190234jsn31508fe7569a');
+headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+return headers;
+}}),
+    
     endpoints: (builder) => ({
         getSummary: builder.query({
-            query: (params) => 'test'
+            query: (params) => 
+            `/summarize?url=${encodeURIComponent(params.articleUrl)}`
         })
     })
 })
+console.log(2)
+
+export const {useLazyGetSummaryQuery} = articleApi;
